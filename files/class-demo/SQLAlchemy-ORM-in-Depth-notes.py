a7b1b7e8242b0825136baa625761dd43adb9be35
Brian Segers <briansegs@gmail.com>
2020-11-09 20:02:19 -0500

    Add nots

---
+++ files/class-demo/SQLAlchemy-ORM-in-Depth-notes.py
@@ -0,0 +1,8 @@
+# from hello_flask_app import db, Person
+# person = Person(name='Brian')
+# db.session.add(person)
+# db.session.add_all([person1, person2])
+# db.session.commit()
+# Person.query.first()
+# person = Person.query.filter_by(name='Brian').first()
+# person.name
