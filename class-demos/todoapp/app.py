6cfeff27841ef89ce00a577ca3912a7aa3a50397
brian <briansegs@gmail.com>
2020-12-12 12:55:36 -0500

    Add changes for work pc and linux

--- class-demos/todoapp/app.py
+++ class-demos/todoapp/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://brian:admin@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
