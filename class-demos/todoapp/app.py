8956ba1829b90472269832155da40569ab6ce3d7
brian <briansegs@gmail.com>
2020-12-14 11:01:56 -0500

    Add session controllers

--- class-demos/todoapp/app.py
+++ class-demos/todoapp/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import sys
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://brian:admin@localhost:5432/todoapp'
@@ -19,13 +21,23 @@ db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.get_json()['description']
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    return jsonify({
-        'description': todo.description
-    })
+    error = False
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if not error:
+        return jsonify({
+            'description': todo.description
+        })
+
 
 @app.route('/')
 def index():
